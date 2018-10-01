c8185e78bb7f97e1ecae29b96b159a564859c301
Aaron Meihm <ameihm@mozilla.com>
2018-10-01 16:36:40 -0500

    use json for output

--- src/main/java/com/mozilla/secops/HeavyHitters.java
+++ src/main/java/com/mozilla/secops/HeavyHitters.java
@@ -7,25 +7,18 @@ import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition;
-import org.apache.beam.sdk.metrics.Counter;
-import org.apache.beam.sdk.metrics.Distribution;
-import org.apache.beam.sdk.metrics.Metrics;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.Validation.Required;
-import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.Mean;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
 import org.apache.beam.sdk.transforms.windowing.Window;
-import org.apache.beam.sdk.transforms.View;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionView;
@@ -34,6 +27,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.api.services.bigquery.model.TableRow;
+
+import com.google.gson.Gson;
 
 import com.mozilla.secops.parser.Event;
 import com.mozilla.secops.parser.Parser;
@@ -49,17 +44,53 @@ import java.time.ZoneId;
 import java.io.Serializable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.UUID;
 
 
 public class HeavyHitters implements Serializable {
-    private class ParseFn extends DoFn<String,String> {
-        private Parser ep;
+    private static class Result implements Serializable {
+        private final String addr;
+        private final Long count;
+        private final String timestamp;
+        private final Double meanValue;
+        private final Double thresholdMod;
+        private final String id;
 
-        ParseFn() {
+        Result(String addr, Long count, Double mv, Double thresholdMod) {
+            this.addr = addr;
+            this.count = count;
+            this.meanValue = mv;
+            this.thresholdMod = thresholdMod;
+
+            id = UUID.randomUUID().toString();
+
+            // XXX Just set the current timestamp here for now
+            DateTimeFormatter f = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss");
+            timestamp = f.print(new DateTime());
         }
+
+        public String getResultId() {
+            return id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            Result t = (Result)o;
+            return getResultId().equals(t.getResultId());
+        }
+
+        static Result fromKV(KV<String, Long> e, Double mv, Double thresholdMod) {
+            Result ret = new Result(e.getKey(), e.getValue(), mv, thresholdMod);
+            return ret;
+        }
+    }
+
+    private static class ParseFn extends DoFn<String, String> {
+        private Parser ep;
 
         @Setup
         public void Setup() {
+            // The parser is not serializable so initialize it in the setup function
             ep = new Parser();
         }
 
@@ -68,15 +99,25 @@ public class HeavyHitters implements Serializable {
             Event e = ep.parse(c.element());
             if (e.getPayloadType() == Payload.PayloadType.GLB) {
                 GLB g = e.getPayload();
+                // XXX Output with current timestamp for now, this should probably parse the
+                // timestamp out of the GLB record
                 c.outputWithTimestamp(g.getSourceAddress(), new Instant());
             }
         }
     }
 
-    static class AnalyzeFn extends PTransform<PCollection<KV<String, Long>>, PCollection<KV<String, Long>>> {
+    static class AnalyzeFn extends PTransform<PCollection<KV<String, Long>>, PCollection<Result>> {
+        private Double thresholdMod;
+
+        AnalyzeFn(Double thresholdMod) {
+            this.thresholdMod = thresholdMod;
+        }
+
         @Override
-        public PCollection<KV<String, Long>> expand(PCollection<KV<String, Long>> col) {
-            PCollection<Long> counts = col.apply("Extract counts", ParDo.of(
+        public PCollection<Result> expand(PCollection<KV<String, Long>> col) {
+            // Create a PCollectionView that contains the mean value of all request counts
+            // across the window, this gets used as a side input
+            PCollection<Long> counts = col.apply("Get request counts", ParDo.of(
                         new DoFn<KV<String, Long>, Long>() {
                             @ProcessElement
                             public void processElement(ProcessContext c) {
@@ -84,18 +125,19 @@ public class HeavyHitters implements Serializable {
                             }
                         }
                         ));
-            final PCollectionView<Double> meanVal = counts.apply("Mean of counts",
+            final PCollectionView<Double> meanVal = counts.apply("Mean of request counts",
                     Mean.<Long>globally().asSingletonView());
-            PCollection<KV<String, Long>> ret = col.apply(
-                    "Analyze",
-                    ParDo.of(
-                        new DoFn<KV<String, Long>, KV<String,Long>>() {
+
+            // Filter items that do not exceed the mean value with the threshold modifier
+            // applied
+            PCollection<Result> ret = col.apply("Analyze", ParDo.of(
+                        new DoFn<KV<String, Long>, Result>() {
                             @ProcessElement
                             public void processElement(ProcessContext c) {
-                                Double mvThresh = 2.0; // XXX testing set
                                 Double mv = c.sideInput(meanVal);
-                                if (c.element().getValue() > (mv * mvThresh)) {
-                                    c.output(c.element());
+                                Result r = Result.fromKV(c.element(), mv, thresholdMod);
+                                if (c.element().getValue() >= (mv * thresholdMod)) {
+                                    c.output(r);
                                 }
                             }
                         }
@@ -104,22 +146,18 @@ public class HeavyHitters implements Serializable {
         }
     }
 
-    static class FileTransformFn extends DoFn<KV<String,Long>,String> {
+    static class FileTransformFn extends DoFn<Result, String> {
         @ProcessElement
         public void processElement(ProcessContext c) {
-            c.output(c.element().toString());
+            c.output(new Gson().toJson(c.element()));
         }
     }
 
-    static class RowTransformFn extends DoFn<KV<String,Long>,TableRow> {
+    static class RowTransformFn extends DoFn<Result, TableRow> {
         @ProcessElement
         public void processElement(ProcessContext c) {
-            TableRow r = new TableRow();
-            r.set("srcip", c.element().getKey());
-            r.set("count", c.element().getValue());
-            DateTime d = new DateTime();
-            DateTimeFormatter f = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss");
-            r.set("timestamp", f.print(d));
+            Gson g = new Gson();
+            TableRow r = g.fromJson(g.toJson(c.element()), TableRow.class);
             c.output(r);
         }
     }
@@ -141,10 +179,10 @@ public class HeavyHitters implements Serializable {
         String getOutputBigQuery();
         void setOutputBigQuery(String value);
 
-        @Description("Threshold value to use for calculation")
+        @Description("Analysis threshold modifier")
         @Default.Double(75.0)
-        ValueProvider<Double> getThresholdValue();
-        void setThresholdValue(ValueProvider<Double> value);
+        Double getAnalysisThresholdModifier();
+        void setAnalysisThresholdModifier(Double value);
     }
 
     static void runHeavyHitters(HeavyHittersOptions options) {
@@ -152,25 +190,25 @@ public class HeavyHitters implements Serializable {
 
         PCollection<String> input;
         if (options.getInputFile() != null) {
-            input = p.apply("Read input", TextIO.read().from(options.getInputFile()));
+            input = p.apply("Read input from file", TextIO.read().from(options.getInputFile()));
         } else if (options.getInputPubsub() != null) {
-            input = p.apply("Read input", PubsubIO.readStrings()
+            input = p.apply("Read input from pubsub", PubsubIO.readStrings()
                     .fromTopic(options.getInputPubsub()));
         } else {
             throw new IllegalArgumentException("no input specified");
         }
 
-        PCollection<KV<String,Long>> data = input.apply(ParDo.of(new HeavyHitters().new ParseFn()))
-            .apply(Window.<String>into(FixedWindows.of(Duration.standardMinutes(2))))
-            .apply(Count.<String>perElement())
-            .apply(new AnalyzeFn());
+        PCollection<Result> data = input.apply("Parse events", ParDo.of(new ParseFn()))
+            .apply("Window events", Window.<String>into(FixedWindows.of(Duration.standardMinutes(2))))
+            .apply("Per-element count", Count.<String>perElement())
+            .apply("Threshold analysis", new AnalyzeFn(options.getAnalysisThresholdModifier()));
 
         if (options.getOutputFile() != null) {
-            data.apply("Text conversion", ParDo.of(new FileTransformFn()))
-                .apply("Output", TextIO.write().to(options.getOutputFile()));
+            data.apply("Conversion for text output", ParDo.of(new FileTransformFn()))
+                .apply("Output to text file", TextIO.write().to(options.getOutputFile()));
         } else if (options.getOutputBigQuery() != null) {
-            data.apply("Row conversion", ParDo.of(new RowTransformFn()))
-                .apply("BigQuery output", BigQueryIO.writeTableRows()
+            data.apply("BigQuery TableRow conversion", ParDo.of(new RowTransformFn()))
+                .apply("Output to BigQuery", BigQueryIO.writeTableRows()
                         .to(options.getOutputBigQuery())
                         .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_NEVER)
                         .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
